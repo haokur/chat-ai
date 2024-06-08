fdf3115a4be608df7e18a338e561457c296dff8f
AllIsWell <admin@haokur.com>
2024-06-08 12:10:20 +0800

    修改目录

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: function (config) {
     config.output.path = path.resolve(__dirname, 'docs'); // 自定义输出目录
-    config.output.publicPath = '/docs';
+    config.output.publicPath = '/chat-ai';
     return config;
   },
 };
