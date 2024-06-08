2f4fa03bac7a755561d6dbac9969c97bfd59bc13
AllIsWell <admin@haokur.com>
2024-06-08 11:55:47 +0800

    修改打包目录

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  webpack: function (config) {
+    config.output.path = path.resolve(__dirname, 'docs'); // 自定义输出目录
+    return config;
+  },
+};
